755839c504f46ec3db2019f87dee4bedcf24a9d0
kkkkkksssssaaaa <kofc312@naver.com>
2024-09-27 23:43:07 +0900

    feat(first-met-react): (ing) add login form

--- realtime-alarm-system-fe/src/pages/login/Login.tsx
+++ realtime-alarm-system-fe/src/pages/login/Login.tsx
@@ -1,3 +1,25 @@
-const Login = () => {};
+import { Form, Row, Button } from "react-bootstrap";
+
+const Login = () => {
+  const handleSubmit = () => {
+    console.log("call handle submit");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Form.Label>아이디</Form.Label>
+        <Form.Control type={"text"} />
+      </Row>
+      <Row>
+        <Form.Label>비밀번호</Form.Label>
+        <Form.Control type={"password"} />
+      </Row>
+      <Row>
+        <Button type={"submit"}>로그인</Button>
+      </Row>
+    </Form>
+  );
+};
 
 export default Login;
---
+++ realtime-alarm-system-fe/src/pages/main/Main.tsx
@@ -0,0 +1,12 @@
+import Login from "../login/Login";
+
+const Main = () => {
+  return (
+    <div>
+      this is main
+      <Login />
+    </div>
+  );
+};
+
+export default Main;
