d6f85ad9a804551bfbe1cc4afbe5f5d40f6347bf
kkkkkksssssaaaa <kofc312@naver.com>
2024-11-09 23:11:05 +0900

    feat(realtime-alarm-system): add my friend list at main

--- realtime-alarm-system-fe/src/pages/main/Main.tsx
+++ realtime-alarm-system-fe/src/pages/main/Main.tsx
@@ -11,6 +11,8 @@ import Bottom from "../../components/common/Bottom";
 const Main = () => {
   const navigate = useNavigate();
   const [artists, setArtists] = useState<Artist[] | undefined>(undefined);
+  const [isContainSubscribeArtist, setIsContainSubscribeArtist] =
+    useState(false);
   const { findAllArtistRequest } = useFindAllArtistRequest();
 
   useEffect(() => {
@@ -25,6 +27,10 @@ const Main = () => {
       findAllArtistRequest()
         .then((res: AxiosResponse) => {
           setArtists(res.data);
+
+          if ((res.data as Artist[]).find((artist) => artist.isSubscribed)) {
+            setIsContainSubscribeArtist(true);
+          }
         })
         .catch((err) => {
           // alert("Cannot find artists. Please re-try later.");
@@ -41,11 +47,23 @@ const Main = () => {
         <span>내 프로필</span>
         <MyProfileListItem />
       </Row>
+      {isContainSubscribeArtist && (
+        <Row>
+          <span>내 친구</span>
+          {artists
+            ?.filter((artist) => artist.isSubscribed)
+            .map((artist) => (
+              <ArtistListItem key={artist.id} {...artist} />
+            ))}
+        </Row>
+      )}
       <Row>
         <span>추천 친구</span>
-        {artists?.map((artist) => (
-          <ArtistListItem key={artist.id} {...artist} />
-        ))}
+        {artists
+          ?.filter((artist) => !artist.isSubscribed)
+          .map((artist) => (
+            <ArtistListItem key={artist.id} {...artist} />
+          ))}
       </Row>
       <Bottom />
     </Container>
